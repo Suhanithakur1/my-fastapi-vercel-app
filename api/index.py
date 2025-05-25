6edc0ec96dd9ddec45fb571da033e7bc462c4644
Suhanithakur1 <148197641+Suhanithakur1@users.noreply.github.com>
2025-05-25 12:06:39 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -18,7 +18,7 @@ app.add_middleware(
 with open("marks.json", "r") as f:
     marks_data = json.load(f)
 
-@app.get("/api")
+@app.get("/")
 async def get_marks(request: Request):
     names = request.query_params.getlist("name")
     result = [marks_data.get(name, None) for name in names]
